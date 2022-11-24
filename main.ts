5f42b7350a63e291829d1486725b227437b25106
kwongchinfung2a13 <115674616+kwongchinfung2a13@users.noreply.github.com>
2022-11-24 18:15:19 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+})
